08342427814ae4e7f301bd73e35a93a9048e7a2e
simonbang <simo12k1@stud.kea.dk>
2021-11-26 09:38:45 +0100

    Oprettet alle klasser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,14 @@
 package com.company;
 
-//hej med jer gruppe 3
+
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
 
-        System.out.println("Heeej");
-
-        System.out.println("Hej fra Mohammad");
     }
 }
+
+
--- src/com/company/Regular.java
+++ src/com/company/Regular.java
@@ -1,4 +1,4 @@
 package com.company;
 
-public class Medlem {
+public class Regular {
 }
