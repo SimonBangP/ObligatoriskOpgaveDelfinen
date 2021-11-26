3fb590ce77559d2f934a8f1fa9c4a3d2d40d472a
SkibstedN <niko933n@stud.kea.dk>
2021-11-26 18:31:59 +0100

    Subscription class name changed to SubscriptionManager. Instance method getSubscriptionSum() implemented.
    Instance of SubscriptionManager created in main.
    Instance method called on the object in main.
    
    +kommentarer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,10 +13,23 @@ public static void main(String[] args) {
 
         Member Simon = new Member("Simon", "bang", 25, 12345, "sbp@gagag", 1600);
         Competitive Nikolaj = new Competitive("Nikolaj", "Skibsted", 47, 123456, "Skibstedn@dasdas", 1600, "Crawl", 800, 05.10, "25/11-21" );
-
+        Regular Sille = new Regular("Sille", "løkke", 23, 384920, "sille@lol.com",1600,"Active");
+        Member Johnny = new Member("Johnny","Svendsen",65,4545345,"johnSvend@lol.com",1200);
+        Competitive Mohammad = new Competitive("Mohammad", "Fadhil", 33, 29346, "MoMo@lol.com", 1600, "Backcrawl", 400,03.05,"22/10-21");
+        Regular Mette = new Regular("Mette","Frederiksen",85,23784,"levmeddet@borgen.dk", 500,"passive");
 
         newMember.add(Simon);
         newCompMember.add(Nikolaj);
+        newRegMember.add(Sille);
+        newMember.add(Johnny);
+        newCompMember.add(Mohammad);
+        newRegMember.add(Mette);
+
+        //Opretter et Subscription objekt  og kalder instance metoden getSubscriptionSum på listerne
+        SubscriptionManager totalSubscriptions = new SubscriptionManager();
+        int S = totalSubscriptions.getSubscriptionSum(newMember, newCompMember, newRegMember);
+        System.out.println("Total income from subscriptions : " + S + "Kr");
+        //Skriver return værdien(den samlede sum af kontingenter) i konsollen
 
         TrainingResultManager resultManager = new TrainingResultManager();
 
---
+++ src/com/company/SubscriptionManager.java
@@ -0,0 +1,27 @@
+//Author @ Nikolaj Skibsted
+
+package com.company;
+
+import java.util.ArrayList;
+
+public class SubscriptionManager {
+
+    private int sum1,sum2,sum3 = 0;
+
+    //traverserer hver Arraylist og returnerer summen af alle kontingenter
+    public int getSubscriptionSum(ArrayList<Member> newMember, ArrayList<Competitive> newCompMember, ArrayList<Regular> newRegMember) {
+        for (int i = 0; i < newMember.size(); i++) {
+            int a = newMember.get(i).getSubscriptionAmount();
+            sum1 += a;
+        }
+        for (int i = 0; i < newCompMember.size(); i++) {
+           int b =  newCompMember.get(i).getSubscriptionAmount();
+            sum2 += b;
+        }
+        for (int i = 0; i < newRegMember.size(); i++) {
+            int c =  newRegMember.get(i).getSubscriptionAmount();
+            sum3 += c;
+        }
+        return sum1+sum2+sum3;
+    }
+}
