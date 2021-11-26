27dc45a444fdf03ea88d01b058a7e1caf572c83b
SkibstedN <niko933n@stud.kea.dk>
2021-11-26 10:46:45 +0100

    Competitive class done

--- src/com/company/Competitive.java
+++ src/com/company/Competitive.java
@@ -2,19 +2,20 @@
 
 package com.company;
 
-public class Competitive  {
+public class Competitive  extends Member{
     protected String discipline;
     protected double bestTime;
     protected String date;
     protected int distance;
 
-  /*  public Competitive(String discipline, double bestTime, String date, int distance) {
-
+    public Competitive(String surName, String lastName, int age, int phoneNumber, String email, int subscriptionAmount) {
+        super(surName, lastName, age, phoneNumber, email, subscriptionAmount);
         this.discipline = discipline;
         this.bestTime = bestTime;
         this.date = date;
         this.distance = distance;
-    }*/
+    }
+
 
     public String getDiscipline() {
         return discipline;
@@ -46,5 +47,15 @@ public class Competitive  {
 
     public void setDistance(int distance) {
         this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Competitive{" +
+                "discipline='" + discipline + '\'' +
+                ", bestTime=" + bestTime +
+                ", date='" + date + '\'' +
+                ", distance=" + distance +
+                '}';
     }
 }
