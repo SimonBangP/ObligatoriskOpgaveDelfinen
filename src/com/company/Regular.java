e5f96308d8d83be2ad1df66d1561e06681626cbe
moha <moha729g@stud.kea.dk>
2021-11-26 10:50:06 +0100

    RegularMember is ready

--- src/com/company/Regular.java
+++ src/com/company/Regular.java
@@ -1,4 +1,17 @@
 package com.company;
 
-public class Regular {
+public class Regular extends Member{
+
+    protected String activityForm;
+
+
+    public Regular(String surName, String lastName, int age, int phoneNumber, String email, int subscriptionAmount, String activityForm) {
+        super(surName, lastName, age, phoneNumber, email, subscriptionAmount);
+        this.activityForm = activityForm;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+" activityForm=" + activityForm;
+    }
 }
