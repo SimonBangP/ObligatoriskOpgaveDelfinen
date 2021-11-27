93ee94d54a89e00166b82f56ac0e74f6517fc1c1
simonbang <simo12k1@stud.kea.dk>
2021-11-27 10:53:21 +0100

    Oprettet alle klasser Fjernet KontigentSum klassen Oprettet Member med constructor og toString
    Rettet i Competitive toString metode.
    
    Tilføjet test til Arraylisterne

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 
         Member Simon = new Member("Simon", "bang", 25, 12345, "sbp@gagag", 1600);
         Competitive Nikolaj = new Competitive("Nikolaj", "Skibsted", 47, 123456, "Skibstedn@dasdas", 1600, "Crawl", 800, 05.10, "25/11-21" );
-
+        Regular Mo = new Regular("Mo", "Stack", 32, 988775, "moStack@gam", 500, "Passive");
 
         newMember.add(Simon);
 
