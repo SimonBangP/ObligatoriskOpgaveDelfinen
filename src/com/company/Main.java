a41f5200013a85fc79f0e6fcf7cbd8256a7d1399
SkibstedN <niko933n@stud.kea.dk>
2021-11-29 11:05:56 +0100

    + read and write for Member arraylist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,31 +2,27 @@ package com.company;
 
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 
 public static void main(String[] args) {
-        MemberManager memberManager = new MemberManager();
-        TrainingResultManager trainingResultManager = new TrainingResultManager();
-        SubscriptionManager subscriptionManager = new SubscriptionManager();
-
-
-        ListManager listManager = new ListManager();
+        MemberManager createMember = new MemberManager();
         Scanner console = new Scanner(System.in);
+        ArrayList<Member> newMember = new ArrayList<Member>();
+        ArrayList<Competitive> newCompMember = new ArrayList<>();
+        ArrayList<Regular> newRegMember = new ArrayList<>();
+
+        Member Simon = new Member("Simon", "bang", 25, 12345, "sbp@gagag", 1600);
+        Competitive Martin = new Competitive("Martin", "Smith", 38, 123456, "msmith@dasdas", 1600, "Crawl", 800, 05.10, "25/11-21" );
+        Regular Mo = new Regular("Mo", "Stack", 32, 988775, "moStack@gam", 500, "Passive");
+
+        newMember.add(Simon);
+
+        createMember.createMember(console, newMember, newCompMember, newRegMember);
 
 
-        Member Simon = new Member("Simon", "bang", 25, 12345, "sbp@gagag", "active", 1600);
-        Competitive Martin = new Competitive("Martin", "Smith", 38, 123456, "msmith@dasdas", "active", 1600, "Crawl", 800, 05.10, "25/11-21" );
-        Regular Mo = new Regular("Mo", "Stack", 32, 988775, "moStack@gam", "passive", 500/*, "Passive"*/);
-
-        listManager.newMember.add(Simon);
-        listManager.newCompMember.add(Martin);
-        listManager.newRegMember.add(Mo);
-
-        memberManager.createMember(console, listManager.newMember, listManager.newCompMember, listManager.newRegMember);
 
     }
 }
