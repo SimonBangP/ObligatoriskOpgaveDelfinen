f4d49970ffc542f1cf193b9e92640384d94ea824
moha <moha729g@stud.kea.dk>
2021-11-29 11:08:22 +0100

    Mo's changes 29/11/2021

--- src/com/company/TrainingResultManager.java
+++ src/com/company/TrainingResultManager.java
@@ -7,18 +7,20 @@ public class TrainingResultManager {
     public void updateBestTime(Scanner console,ArrayList<Competitive> newCompMember){
         //Scanner scan = new Scanner(System.in);
         for(int i = 0; i < newCompMember.size(); i++){
-            System.out.println((i+ 1)+": "+newCompMember.get(i).getFirstName()+
-                    newCompMember.get(i).getLastName()+newCompMember.get(i).getDiscipline()+
-                    newCompMember.get(i).getDistance());
+            System.out.println((i+ 1)+": "+newCompMember.get(i).getFirstName()+" "+
+                    newCompMember.get(i).getLastName()+" "+newCompMember.get(i).getDiscipline()+" "+
+                    newCompMember.get(i).getDistance()+" "+newCompMember.get(i).getBestTime()+" "+
+                    newCompMember.get(i).getDate());
         }
         //Tilføj System.out.print og bed brugeren indtaste hvilken medlem der skal ændres.
-
+        System.out.println("Choose the swimmer which result you'd like to update!");
         int indexOfCompetitive = console.nextInt();
         System.out.println(newCompMember.get(indexOfCompetitive - 1));
 
         System.out.println("Enter new BestTime");
         double bestTime = console.nextDouble();
 
+        //kan man evt. bruge local time feature i den sammenhæng???
         System.out.println("Enter the date");
         String date = console.next();
 
