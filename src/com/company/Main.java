5ad9566d55cbc6d76a3b86ee7d195ef9c825a663
moha <moha729g@stud.kea.dk>
2021-11-30 17:55:27 +0100

    Mo's changes 30/11/2021 - working on adding a list of results to each competetor(2)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public static void main(String[] args) throws FileNotFoundException {
         ListManager listManager = new ListManager();
         SubscriptionManager subscriptionManager = new SubscriptionManager();
 
-        Competitive Bob = new Competitive("Bob", "Johnson", 33, 34762378, "Bob@lol.com","active",1600,"crawl", 800, 07.10, "11/09-20");
+        Competitive Bob = new Competitive("Bob", "Johnson", 33, 34762378, "Bob@lol.com","active",1600,"crawl", 800/*, 07.10, "11/09-20"*/);
         Member Kim = new Member("Kim", "Larsen",62, 34873847,"Kim@lol.com", "active", 1200);
         Regular Liz = new Regular("Liz","Smith", 35, 34748569, "Liz@lol.com","active", 1600, 3, "crawl");
         Member John = new Member("John", "Smith", 52, 36363636, "John@lol.com","Passive",500);
