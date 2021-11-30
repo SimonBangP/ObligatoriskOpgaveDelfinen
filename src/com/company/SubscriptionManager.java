349a81290f904e6f9f97e242512c89d14e0a752d
SkibstedN <niko933n@stud.kea.dk>
2021-11-30 13:29:40 +0100

    tilføjet objekter i main

--- src/com/company/SubscriptionManager.java
+++ src/com/company/SubscriptionManager.java
@@ -6,22 +6,22 @@ import java.util.ArrayList;
 
 public class SubscriptionManager {
 
-    private int sum1/*,sum2,sum3*/ = 0;
+    private int sum1,sum2,sum3 = 0;
 
     //traverserer Arraylist<Member> og returnerer summen af alle kontingenterne
-    public int getSubscriptionSum(ArrayList<Member> newMember/*, ArrayList<Competitive> newCompMember, ArrayList<Regular> newRegMember*/) {
+    public int getSubscriptionSum(ArrayList<Member> newMember, ArrayList<Competitive> newCompMember, ArrayList<Regular> newRegMember) {
         for (int i = 0; i < newMember.size(); i++) {
             int membershipFee = newMember.get(i).getSubscriptionAmount();
             sum1 += membershipFee;
         }
-        /*for (int i = 0; i < newCompMember.size(); i++) {
+        for (int i = 0; i < newCompMember.size(); i++) {
            int b =  newCompMember.get(i).getSubscriptionAmount();
             sum2 += b;
         }
         for (int i = 0; i < newRegMember.size(); i++) {
             int c =  newRegMember.get(i).getSubscriptionAmount();
             sum3 += c;
-        }*/
-        return sum1/*+sum2+sum3*/;
+        }
+        return sum1+sum2+sum3;
     }
 }
